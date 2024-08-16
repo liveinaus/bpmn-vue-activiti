8f62ce56bb09cbac5a5ca8689177f7f8720e4169
Sam Wang <admin@liveinaus.com>
2024-08-16 23:03:20 +1000

    refactor: globally code reformat

--- src/bpmn/config/modules/Process.tsx
+++ src/bpmn/config/modules/Process.tsx
@@ -1,28 +1,28 @@
 import {
-    CommonGroupProperties,
-    DocumentGroupProperties,
-    ExtensionGroupProperties,
-    getElementTypeListenerProperties,
-} from "../common";
+  CommonGroupProperties,
+  DocumentGroupProperties,
+  ExtensionGroupProperties,
+  getElementTypeListenerProperties,
+} from '../common';
 
 //流程数据属性配置数组
 const ProcessGroupPropertiesArray = [
-    CommonGroupProperties,
-    /**
-     * 监听器组
-     */
-    getElementTypeListenerProperties({
-        name: "Global Listeners",
-    }),
-    ExtensionGroupProperties,
-    DocumentGroupProperties,
+  CommonGroupProperties,
+  /**
+   * 监听器组
+   */
+  getElementTypeListenerProperties({
+    name: 'Global Listeners',
+  }),
+  ExtensionGroupProperties,
+  DocumentGroupProperties,
 ];
 
 export default {
-    //流程
-    "bpmn:Process": ProcessGroupPropertiesArray,
-    //子流程
-    "bpmn:SubProcess": ProcessGroupPropertiesArray,
-    //转运
-    "bpmn:Transaction": ProcessGroupPropertiesArray,
+  //流程
+  'bpmn:Process': ProcessGroupPropertiesArray,
+  //子流程
+  'bpmn:SubProcess': ProcessGroupPropertiesArray,
+  //转运
+  'bpmn:Transaction': ProcessGroupPropertiesArray,
 };
--- src/components/modeler/Modeler.tsx
+++ src/components/modeler/Modeler.tsx
@@ -1,55 +1,55 @@
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css";
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
-import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
-import "bpmn-js/dist/assets/diagram-js.css";
-import "./modeler.css";
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
+import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
+import 'bpmn-js/dist/assets/diagram-js.css';
+import './modeler.css';
 
-import { BpmnStore } from "@/bpmn/store";
-import { BpmnContext } from "@/bpmn/type";
-import { defineComponent, onMounted } from "vue";
-import translate from "../../bpmn/i18n";
-import activitiModdel from "../../bpmn/resources/activiti-moddel.json";
+import { BpmnStore } from '@/bpmn/store';
+import { BpmnContext } from '@/bpmn/type';
+import { defineComponent, onMounted } from 'vue';
+import translate from '../../bpmn/i18n';
+import activitiModdel from '../../bpmn/resources/activiti-moddel.json';
 
 export default defineComponent({
-    name: "Modeler",
-    props: {
-        bpmnXml: {
-            type: String,
-            required: true,
-        },
+  name: 'Modeler',
+  props: {
+    bpmnXml: {
+      type: String,
+      required: true,
     },
-    emits: ["elementChanged"],
-    setup(props, { emit }) {
-        const bpmnContext: BpmnContext = BpmnStore;
-        onMounted(() => {
-            bpmnContext.initModeler({
-                container: "#modeler-container",
-                additionalModules: [
-                    //添加翻译
-                    { translate: ["value", translate("en")] },
-                ],
-                moddleExtensions: {
-                    activiti: activitiModdel,
-                },
-            });
+  },
+  emits: ['elementChanged'],
+  setup(props, { emit }) {
+    const bpmnContext: BpmnContext = BpmnStore;
+    onMounted(() => {
+      bpmnContext.initModeler({
+        container: '#modeler-container',
+        additionalModules: [
+          //添加翻译
+          { translate: ['value', translate('en')] },
+        ],
+        moddleExtensions: {
+          activiti: activitiModdel,
+        },
+      });
 
-            bpmnContext
-                .importXML(props.bpmnXml)
-                .then((result: Array<string>) => {
-                    if (result.length) {
-                        console.warn("importSuccess warnings", result);
-                    }
-                })
-                .catch((err: any) => {
-                    console.warn("importFail errors ", err);
-                });
-
-            bpmnContext.addEventListener("element.changed", async (evt) => {
-                const { xml } = await bpmnContext.getXML();
-                emit("elementChanged", xml);
-            });
+      bpmnContext
+        .importXML(props.bpmnXml)
+        .then((result: Array<string>) => {
+          if (result.length) {
+            console.warn('importSuccess warnings', result);
+          }
+        })
+        .catch((err: any) => {
+          console.warn('importFail errors ', err);
         });
 
-        return () => <div id="modeler-container" />;
-    },
+      bpmnContext.addEventListener('element.changed', async (evt) => {
+        const { xml } = await bpmnContext.getXML();
+        emit('elementChanged', xml);
+      });
+    });
+
+    return () => <div id="modeler-container" />;
+  },
 });
